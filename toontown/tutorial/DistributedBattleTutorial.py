8db619bcbfb78c9c3f0b853d3289a4f733c9be96
BatteryRock <108632596+BatteryRock@users.noreply.github.com>
2024-03-07 10:26:59 -0500

    add back the timer

--- toontown/tutorial/DistributedBattleTutorial.py
+++ toontown/tutorial/DistributedBattleTutorial.py
@@ -4,8 +4,5 @@ from direct.directnotify import DirectNotifyGlobal
 class DistributedBattleTutorial(DistributedBattle.DistributedBattle):
     notify = DirectNotifyGlobal.directNotify.newCategory('DistributedBattleTutorial')
 
-    def startTimer(self, ts = 0):
-        self.townBattle.timer.hide()
-
     def playReward(self, ts):
         self.movie.playTutorialReward(ts, self.uniqueName('reward'), self.handleRewardDone)
