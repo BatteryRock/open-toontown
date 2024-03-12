344cb6d44ab56656f8073c1cc9ebb68976168a60
BatteryRock <108632596+BatteryRock@users.noreply.github.com>
2024-03-11 20:48:16 -0400

    shouldn't take 4 seconds

--- win32/startall.bat
+++ win32/startall.bat
@@ -2,7 +2,7 @@
 start "Starting Astron..." "start_astron_server.bat"
 timeout /t 1 >nul
 start "Starting UberDOG..." "start_uberdog_server.bat"
-timeout /t 4 >nul
+timeout /t 3 >nul
 start "Starting AI..." "start_ai_server.bat"
-timeout /t 4 >nul
+timeout /t 3 >nul
 start "Starting the game..." "start_game.bat"
